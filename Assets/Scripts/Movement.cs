8e39978162b2e2f0f52fd9e87fb8b5594d9abb22
Taib <taibkurbanaliev@gmail.com>
2023-01-05 14:45:28 +0400

    Change

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
+    private const string IsRun = "IsRun";
+    private const string Jump = "Jump";
+
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
 
-    [SerializeField] private bool _canJump;
+    private bool _canJump;
     private Animator _animatitor;
     private Rigidbody2D _rigidbody2D;
 
@@ -22,23 +27,23 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(_speed * Time.deltaTime, 0, 0);
-            _animatitor.SetBool("IsRun",true);
+            _animatitor.SetBool(IsRun, true);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(_speed * -Time.deltaTime, 0, 0);
-            _animatitor.SetBool("IsRun", true);
+            _animatitor.SetBool(IsRun, true);
         }
         else
         {
-            _animatitor.SetBool("IsRun", false);
+            _animatitor.SetBool(IsRun, false);
         }
         
         if (_canJump && Input.GetKey(KeyCode.Space))
         {
             _canJump = false;
             _rigidbody2D.AddForce(Vector2.up * _jumpForce);
-            _animatitor.SetTrigger("Jump");
+            _animatitor.SetTrigger(Jump);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
